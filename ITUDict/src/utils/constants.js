c55479fb484a012324c9589566e636a868f642d8
marifdemirtas <12948961+marifdemirtas@users.noreply.github.com>
2023-01-09 23:48:27 +0300

    API Gateway integration

--- ITUDict/src/utils/constants.js
+++ ITUDict/src/utils/constants.js
@@ -1,2 +1,2 @@
 // export const BACKEND_API_URL = "http://itudict-be.us-east-1.elasticbeanstalk.com";
-export const BACKEND_API_URL = "http://192.168.1.244:4000";
+export const BACKEND_API_URL = "https://94l5ddyc16.execute-api.us-east-1.amazonaws.com";
